ce23f62d54682f180eb4de63ad8a3ed6695631bc
Elvan Ersöz <elvanersoz@outlook.com>
2023-09-14 17:00:37 +0300

    sing up page modified

--- hoaxifyFrontend/src/main.jsx
+++ hoaxifyFrontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 import {SingUp} from "./pages/SingUp/index.jsx";
+import "./styles.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- hoaxifyFrontend/src/pages/SingUp/index.jsx
+++ hoaxifyFrontend/src/pages/SingUp/index.jsx
@@ -6,45 +6,79 @@ export function SingUp() {
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const [passwordRepeat, setPasswordRepeat] = useState()
+    const [apiProgress, setApiProgress] = useState(false);
+    const [successMessage, setSuccasseMessage] = useState();
 
     const buttonDisable = () => {
         if (!username || !email || !password || password !== passwordRepeat) {
             return true
         }
     }
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
-        axios.post('/api/v1/users', {
+        setApiProgress(true);
+        setSuccasseMessage();
+
+        await axios.post('/api/v1/users', {
             username: username,
             email: email,
             password: password
+        }).then((response) => {
+            setSuccasseMessage(response.data.username + " has been successfully registered")
+        }).finally(() => {
+            setApiProgress(false)
         })
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <h1>Sing Up</h1>
-            <div>
-                <label htmlFor="username">Username</label>
-                <input id="username"
-                       onChange={(event) => setUsername(event.target.value)}/>
+        <div className="container">
+            <div className="col-xl-8 offset-xl-2">
+                <form className="card" onSubmit={onSubmit}>
+                    <div className="text-center card-header">
+                        <h1>Sing Up</h1>
+                    </div>
+                    <div className="card-body">
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">Username</label>
+                            <input id="username"
+                                   className="form-control"
+                                   onChange={(event) => setUsername(event.target.value)}/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">E-mail</label>
+                            <input id="email"
+                                   className="form-control"
+                                   onChange={(event) => setEmail(event.target.value)}/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input id="password"
+                                   type="password"
+                                   className="form-control"
+                                   onChange={(event) => setPassword(event.target.value)}/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="passwordRepeat" className="form-label">Password Repeat</label>
+                            <input id="passwordRepeat"
+                                   type="password"
+                                   className="form-control"
+                                   onChange={(event) => setPasswordRepeat(event.target.value)}/>
+                        </div>
+
+                        {successMessage && <div className="alert alert-success">
+                            {successMessage}
+                        </div>}
+
+                        <div className="text-center">
+                            <button className="btn btn-primary"
+                                    disabled={buttonDisable() || apiProgress}>
+                                    {apiProgress && <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>}
+                                Sing Up
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label htmlFor="email">E-mail</label>
-                <input id="email"
-                       onChange={(event) => setEmail(event.target.value)}/>
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input id="password" type="password"
-                       onChange={(event) => setPassword(event.target.value)}/>
-            </div>
-            <div>
-                <label htmlFor="passwordRepeat">Password Repeat</label>
-                <input id="passwordRepeat" type="password"
-                       onChange={(event) => setPasswordRepeat(event.target.value)}/>
-            </div>
-            <button disabled={buttonDisable()}>Sing Up</button>
-        </form>
+        </div>
     );
 }
